927b6a8dd6679c66cb71ae4912b184b8eb4e5502
Ivango Wippel <ognavi@gmail.com>
2021-08-10 19:59:01 -0300

    criação do projeto

---
+++ src/main/java/com/ivangowippel/cursoilw/CursoilwApplication.java
@@ -0,0 +1,13 @@
+package com.ivangowippel.cursoilw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoilwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoilwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivangowippel/cursoilw/CursoilwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivangowippel.cursoilw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoilwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bc4dd4cd3bba31790d1d63c2c44ca2f042b213f5
Ivango Wippel <ognavi@gmail.com>
2021-08-11 10:40:21 -0300

    Criação de Estado e Cidade

---
+++ src/main/java/com/ivangowippel/cursoilw/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.ivangowippel.cursoilw.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ivangowippel.cursoilw.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}

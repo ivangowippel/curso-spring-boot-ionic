99a81abbfa089aadee2cc678d89ef198db106d62
Ivango Wippel <ognavi@gmail.com>
2021-08-11 19:21:23 -0300

    Exclusão de uma Categoria com DELETE

--- src/main/java/com/ivangowippel/cursoilw/services/CategoriaService.java
+++ src/main/java/com/ivangowippel/cursoilw/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ivangowippel.cursoilw.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ivangowippel.cursoilw.domain.Categoria;
 import com.ivangowippel.cursoilw.repositories.CategoriaRepository;
+import com.ivangowippel.cursoilw.services.exceptions.DataIntegrityException;
 import com.ivangowippel.cursoilw.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }

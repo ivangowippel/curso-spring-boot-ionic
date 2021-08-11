487daf2ca2a2411d2b21c62ec6fb84a75a930946
Ivango Wippel <ognavi@gmail.com>
2021-08-11 19:48:11 -0300

    Listagem de todas Categorias com DTO

--- src/main/java/com/ivangowippel/cursoilw/resources/CategoriaResource.java
+++ src/main/java/com/ivangowippel/cursoilw/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.ivangowippel.cursoilw.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ivangowippel.cursoilw.domain.Categoria;
+import com.ivangowippel.cursoilw.dto.CategoriaDTO;
 import com.ivangowippel.cursoilw.services.CategoriaService;
 
 @RestController
@@ -47,5 +50,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	
 	
 }
